36d6892697f8130421138a38ff13311093eb9b5f
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-03-02 17:19:17 -0500

    Feat: Modificación de Track entity

--- src/domain/entities/Track.ts
+++ src/domain/entities/Track.ts
@@ -1,16 +1,18 @@
-export interface RawTrack {
-  id: string;   
-  url: string; 
-  title: string | null;    
-  artist: string | null; 
-  album: string | null;  
-  genre: string | null; 
-  year: number | null;  
-  duration: number;
-  fileName: string;     
-  fileSize: number;   
+export interface Track {
+  readonly id: string; 
+  readonly url: string; 
+  title: string;         
+  artistName: string;      
+  albumName: string;  
+  duration: number;        
   format: string;    
-  bitrate: number | null;
-  artworkUri: string | null;
-  dateAdded: number;
+  bitrate?: number;  
+  size: number;   
+  genre?: string;  
+  year?: number;  
+  trackNumber?: number;   
+  artworkUri?: string | null;  
+  isProcessed: boolean;  
+  dateAdded: number;         
+  lastPlayed?: number;  
 }
