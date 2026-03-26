3ae5da4fe05d86d691b689b99b6a6fb741535676
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-03-25 21:10:13 -0500

    Feat: Cambios de atributos en entidad de album

--- src/domain/entities/album.entity.ts
+++ src/domain/entities/album.entity.ts
@@ -2,9 +2,10 @@ export interface Album {
   readonly id: string;
   title: string;
   artistId: string;
-  artistName: string;
+  artistName: string; 
   artworkUri?: string | null;
   year?: number | null;
-  genre?: string | null;
   trackCount: number;
+  isCompilation: boolean;
+  folderPath?: string;
 }
