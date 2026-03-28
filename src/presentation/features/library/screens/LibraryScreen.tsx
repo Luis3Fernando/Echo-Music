31024acefb599d41443dff3180752086c867fba2
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-03-27 23:41:46 -0500

    Feat: Listado de playlist del sistema

--- src/presentation/features/library/screens/LibraryScreen.tsx
+++ src/presentation/features/library/screens/LibraryScreen.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, View, Platform, ScrollView } from "react-native";
+import { StyleSheet, View, Platform, ScrollView, ActivityIndicator } from "react-native";
 import { Colors } from "@theme/colors";
-import { SYSTEM_PLAYLISTS, USER_PLAYLISTS } from "@mocks/mock-playlists";
 import ScreenHeader from "@components/organisms/ScreenHeader";
 import PlaylistHorizontalList from "../components/PlaylistHorizontalList";
 import FolderListSection from "../components/FolderListSection";
 import { useNavigation } from "@react-navigation/native";
+import { usePlaylists } from "@hooks/use-playlists"; 
 
 const LibraryScreen = () => {
   const navigation = useNavigation<any>();
+  const { systemPlaylists, userPlaylists, isLoading } = usePlaylists();
+
+  if (isLoading) {
+    return (
+      <View style={[styles.container, { justifyContent: 'center' }]}>
+        <ActivityIndicator color={Colors.primary} size="large" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -22,14 +31,18 @@ const LibraryScreen = () => {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 140 }}
       >
-        <PlaylistHorizontalList
-          sectionTitle="Tus listas de reproducción"
-          data={USER_PLAYLISTS}
-        />
-        <PlaylistHorizontalList
-          sectionTitle="Para tí"
-          data={SYSTEM_PLAYLISTS}
-        />
+        {userPlaylists.length > 0 && (
+          <PlaylistHorizontalList
+            sectionTitle="Tus listas de reproducción"
+            data={userPlaylists}
+          />
+        )}
+        {systemPlaylists.length > 0 && (
+          <PlaylistHorizontalList
+            sectionTitle="Para tí"
+            data={systemPlaylists}
+          />
+        )}
         <FolderListSection />
       </ScrollView>
     </View>
